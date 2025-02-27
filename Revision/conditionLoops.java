4b033b96169a3805259b2324081ce531257c7d78
Hasan <codesmaverick@gmail.com>
2025-02-27 16:52:15 +0530

    day 2.2

---
+++ Revision/conditionLoops.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+public class conditionLoops{
+        public static Scanner sc=new Scanner(System.in);
+        public static void main(String[] args) {
+
+                // Basics 
+                // int n=sc.nextInt();
+                // int ans=subtractProductAndSum(n);
+                // System.out.println(ans);
+
+                // int num=sc.nextInt();
+                // factorsOfNum(num);
+
+                // printSumofAll();
+                // maxOfAll();
+
+                // Intermediate Level 
+                
+
+                sc.close();
+        }
+
+        // From Q-22
+        // public static int subtractProductAndSum(int n) {
+        //         int prod=1,sum=0;
+        //         while(n!=0){
+        //             int rem=n%10;
+        //             prod *= rem;
+        //             sum += rem;
+        //             n=n/10;
+        //         }
+        //         return prod-sum;
+        //     }
+
+        // public static void factorsOfNum(int num){
+        //         for(int i=1;i<=num;i++){
+        //                 if(num%i==0){
+        //                         System.out.print(i+" ");
+        //                 }
+        //         }
+        // }
+
+        // public static void printSumofAll(){
+        //         int sum=0;
+        //         while(sc.hasNextInt()){
+        //                 int n=sc.nextInt();
+        //                 if(n==0){
+        //                         break;
+        //                 }
+        //                 sum+=n;
+        //         }
+        //         System.out.println(sum);
+        // }
+
+        // public static void maxOfAll(){
+        //         int max=Integer.MIN_VALUE;
+        //         while(sc.hasNextInt()){
+        //                 int num=sc.nextInt();
+        //                 if(num==0){
+        //                         break;
+        //                 }
+        //                 if(num>max){
+        //                         max=num;
+        //                 }
+        //         }
+        //         System.out.println(max);
+        // }
+
+
+
+}
