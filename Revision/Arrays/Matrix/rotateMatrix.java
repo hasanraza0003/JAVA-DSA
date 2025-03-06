4100b31b12d6dfadae3e49f6c6d35625a6459824
Hasan <codesmaverick@gmail.com>
2025-03-06 17:37:47 +0530

    Matrix Rotation

---
+++ Revision/Arrays/Matrix/rotateMatrix.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+public class rotateMatrix {
+        public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter the matrix dimension -> Row then Col -> ");
+                int row = sc.nextInt();
+                int col = sc.nextInt();
+
+                int mat[][] = new int[row][col];
+                int mat2[][] = new int[row][col];
+
+                for (int i = 0; i < row; i++) {
+                        for (int j = 0; j < col; j++) {
+                                mat[i][j] = sc.nextInt();
+                                mat2[i][j] = mat[i][j];
+                        }
+                }
+
+                rotateClockWise(mat, row, col);
+                rotateCounterClockWise(mat2,row,col);
+
+                for (int i = 0; i < row; i++) {
+                        for (int j = 0; j < col; j++) {
+                                System.out.print(mat[i][j] + " ");
+                        }
+                        System.out.println();
+                }
+                for (int i = 0; i < row; i++) {
+                        for (int j = 0; j < col; j++) {
+                                System.out.print(mat2[i][j] + " ");
+                        }
+                        System.out.println();
+                }
+
+        }
+
+        public static void rotateClockWise(int[][] mat, int row, int col) {
+
+                // transpose
+                for (int i = 0; i < row; i++) {
+                        for (int j = i; j < col; j++) {
+                                int temp = mat[i][j];
+                                mat[i][j] = mat[j][i];
+                                mat[j][i] = temp;
+                        }
+                }
+
+                // reverse
+                for (int i = 0; i < row; i++) {
+                        for (int j = 0; j < col/2; j++) {
+                                int temp = mat[i][j];
+                                mat[i][j] = mat[i][col - 1 - j];
+                                mat[i][col - 1 - j] = temp;
+                        }
+                }
+        }
+        public static void rotateCounterClockWise(int[][] mat, int row, int col) {
+
+                // transpose
+                for (int i = 0; i < row; i++) {
+                        for (int j = i; j < col; j++) {
+                                int temp = mat[i][j];
+                                mat[i][j] = mat[j][i];
+                                mat[j][i] = temp;
+                        }
+                }
+                // reverse
+                for (int i = 0; i < row/2; i++) {
+                        for (int j = 0; j < col ; j++) {
+                                int temp = mat[i][j];
+                                mat[i][j] = mat[col-1-i][j];
+                                mat[col-1-i][j] = temp;
+                        }
+                }
+        }
+
+}
